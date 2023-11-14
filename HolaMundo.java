9fb2a205759363734f01e0567fe7f7dc07753498
alopvaz <alopvaz014@iesalmudeyne.es>
2023-11-14 14:20:16 +0100

    ultimo cambio2

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo{
-    public static void mian(String[] args) {
+    public static void main(String[] args) {
         String rojo = "\033[32m";
         String azul = "\033[34m";
         System.out.println("Hola mundo");

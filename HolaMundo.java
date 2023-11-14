d9d1aabb0a006721787775fd1f4b1d8c73fe66f2
alopvaz <alopvaz014@iesalmudeyne.es>
2023-11-14 14:21:47 +0100

    cambio aplicando variables

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,8 +2,8 @@ public class HolaMundo{
     public static void main(String[] args) {
         String rojo = "\033[32m";
         String azul = "\033[34m";
-        System.out.println("Hola mundo");
-        System.out.println("Haciendo pruebas de git");
+        System.out.println(rojo + "Hola mundo");
+        System.out.println(azul + "Haciendo pruebas de git");
 
     }
 }

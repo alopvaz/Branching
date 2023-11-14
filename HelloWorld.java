e8e3db79152ddf347eb91baffb4ac66484f3c18f
alopvaz <alopvaz014@iesalmudeyne.es>
2023-11-14 14:13:59 +0100

    as

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HolaMundo{
     public static void mian(String[] args) {
+        String rojo = "\033[32m";
+        String azul = "\033[34m";
         System.out.println("Hola mundo");
         System.out.println("Haciendo pruebas de git");
 

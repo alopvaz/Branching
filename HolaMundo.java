c267436d95ffbe533b607bbbf52cce91bbb52843
alopvaz <alopvaz014@iesalmudeyne.es>
2023-11-17 10:39:15 +0100

    Modificando y mostrar estoy en GIT

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,7 @@ public class HolaMundo{
     public static void main(String[] args) {
         String rojo = "\033[31m";
         String azul = "\033[34m";
-        System.out.println(rojo + "Hola mundo");
+        System.out.println(rojo + "Hola mundo estoy en GIT");
         System.out.println(azul + "Haciendo pruebas de git");
 
     }
